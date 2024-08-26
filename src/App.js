f3ed4fa051777a368e8bd5fe90577adb237fe572
mizan <mrahman64926@gmail.com>
2024-08-26 17:11:25 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Board from "./component/Board";
+const App = () => {
+  return (
+    <div className="board">
+      <Board />
+    </div>
+  );
+};
+export default App;
